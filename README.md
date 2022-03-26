3e4b02bc8dea05fee5053af3731c6c0f47270cde
Nonad <NonA_D@126.com>
2022-03-26 16:16:32 +0800

    220326

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # TrSel
+
+https://github.com/dk-liang/TransCrowd
+
+&
+
+https://arxiv.org/abs/2111.11591
+
+Processing videos with opencv, I sew transformer and token selection together to count objects in target videos.
+
+Unfortunately, there are too many parameters of MLP. I am unable to run it on the lab's GPU.
+
+It will work if parameters are tuned, maybe...
+
+Maybe next time)
+
+owo 
+
+NAD 220326
---
+++ test.py
@@ -0,0 +1,257 @@
+from __future__ import division
+import warnings
+from models import base_patch16_384_token, base_patch16_384_gap
+import torch.nn as nn
+from torchvision import transforms
+
+import math
+from utils import save_checkpoint, setup_seed
+import torch
+import os
+import logging
+import nni
+from nni.utils import merge_parameter
+from config import return_args, args
+
+warnings.filterwarnings('ignore')
+import time
+import cv2
+
+setup_seed(args.seed)
+
+logger = logging.getLogger('mnist_AutoML')
+
+
+
+def main(args):
+
+    source = '/home/3.mp4'
+    cap = cv2.VideoCapture(source)
+    if not cap.isOpened():
+        raise IOError("Video {} cannot be opened".format(source))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    nxt, img = cap.read()
+    imglist = list()
+    frame_cnt = 0
+    while(nxt):
+        frame_cnt = frame_cnt + 1
+        if frame_cnt % int(fps / 2) == 0:
+            img = cv2.cvtColor(cv2.resize(img, (384, 384)), cv2.COLOR_BGR2RGB)
+            imglist.append(img)
+        nxt, img = cap.read()
+
+
+    print("Pre_load dataset ......")
+    gt = (int)(os.path.basename(source).split('num', 1)[1].split('.', 1)[0])
+    testdata = {}
+    for j in range(len(imglist)):
+        fname = os.path.basename(source).split('.', 1)[0] + str(j)
+        blob = {}
+        blob['img'] = imglist[j]
+        blob['fname'] = fname
+        testdata[j] = blob
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args['gpu_id']
+
+    if args['model_type'] == 'token':
+        model = base_patch16_384_token(pretrained=True)
+    elif args['model_type'] == 'gap':
+        model = base_patch16_384_gap(pretrained=True)
+
+    model = nn.DataParallel(model, device_ids=[0])
+    model = model.cuda()
+
+    criterion = nn.L1Loss(size_average=False).cuda()
+
+    optimizer = torch.optim.Adam(
+        [  #
+            {'params': model.parameters(), 'lr': args['lr']},
+        ], lr=args['lr'], weight_decay=args['weight_decay'])
+
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[300], gamma=0.1, last_epoch=-1)
+    print(args['pre'])
+
+    # args['save_path'] = args['save_path'] + str(args['rdt'])
+    print(args['save_path'])
+    if not os.path.exists(args['save_path']):
+        os.makedirs(args['save_path'])
+
+    if args['pre']:
+        if os.path.isfile(args['pre']):
+            print("=> loading checkpoint '{}'".format(args['pre']))
+            checkpoint = torch.load(args['pre'])
+            model.load_state_dict(checkpoint['state_dict'], strict=False)
+            args['start_epoch'] = checkpoint['epoch']
+            args['best_pred'] = checkpoint['best_prec1']
+        else:
+            print("=> no checkpoint found at '{}'".format(args['pre']))
+
+    torch.set_num_threads(args['workers'])
+
+    print(args['best_pred'], args['start_epoch'])
+
+    test_data = testdata
+
+    # '''inference'''
+    prec1 = validate(test_data, model, gt, args)
+    #
+    print(' * best MAE {mae:.3f} '.format(mae=args['best_pred']))
+    #
+
+
+
+
+def pre_data(train_list, args, train):
+    print("Pre_load dataset ......")
+    data_keys = {}
+    count = 0
+    for j in range(len(train_list)):
+        Img_path = train_list[j]
+        fname = os.path.basename(Img_path)
+        img, gt_count = load_data(Img_path, args, train)
+
+        blob = {}
+        blob['img'] = img
+        blob['gt_count'] = gt_count
+        blob['fname'] = fname
+        data_keys[count] = blob
+        count += 1
+
+        '''for debug'''
+        # if j> 10:
+        #     break
+    return data_keys
+
+
+def train(Pre_data, model, criterion, optimizer, epoch, args, scheduler):
+    losses = AverageMeter()
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset.listDataset(Pre_data, args['save_path'],
+                            shuffle=True,
+                            transform=transforms.Compose([
+                                transforms.ToTensor(),
+
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                     std=[0.229, 0.224, 0.225]),
+                            ]),
+                            train=True,
+                            batch_size=args['batch_size'],
+                            num_workers=args['workers'],
+                            args=args),
+        batch_size=args['batch_size'], drop_last=False)
+    args['lr'] = optimizer.param_groups[0]['lr']
+    print('epoch %d, processed %d samples, lr %.10f' % (epoch, epoch * len(train_loader.dataset), args['lr']))
+
+    model.train()
+    end = time.time()
+
+    for i, (fname, img, gt_count) in enumerate(train_loader):
+
+        data_time.update(time.time() - end)
+        img = img.cuda()
+
+        out1 = model(img)
+        gt_count = gt_count.type(torch.FloatTensor).cuda().unsqueeze(1)
+
+        # print(out1.shape, kpoint.shape)
+        loss = criterion(out1, gt_count)
+
+        losses.update(loss.item(), img.size(0))
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args['print_freq'] == 0:
+            print('4_Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                .format(
+                epoch, i, len(train_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses))
+
+    scheduler.step()
+
+
+def validate(Pre_data, model, gt_count, args):
+    print('begin test')
+    batch_size = 1
+    test_loader = torch.utils.data.DataLoader(
+        dataset.listDataset(Pre_data, args['save_path'],
+                            shuffle=False,
+                            transform=transforms.Compose([
+                                transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                            std=[0.229, 0.224, 0.225]),
+
+                            ]),
+                            args=args, train=False),
+        batch_size=1)
+
+    model.eval()
+
+    mae = 0.0
+    mse = 0.0
+    visi = []
+    index = 0
+
+    for i, (fname, img) in enumerate(test_loader):
+
+        img = img.cuda()
+        if len(img.shape) == 5:
+            img = img.squeeze(0)
+        if len(img.shape) == 3:
+            img = img.unsqueeze(0)
+
+        with torch.no_grad():
+            out1 = model(img)
+            count = torch.sum(out1).item()
+
+    gt_count = torch.sum(gt_count).item()
+    mae += abs(gt_count - count)
+    mse += abs(gt_count - count) * abs(gt_count - count)
+
+    print('{fname} Gt {gt:.2f} Pred {pred}'.format(fname=fname, gt=gt_count, pred=count))
+
+    mae = mae * 1.0 / (len(test_loader) * batch_size)
+    mse = math.sqrt(mse / (len(test_loader)) * batch_size)
+
+    nni.report_intermediate_result(mae)
+    print(' \n* MAE {mae:.3f}\n'.format(mae=mae), '* MSE {mse:.3f}'.format(mse=mse))
+
+    return mae
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+if __name__ == '__main__':
+    tuner_params = nni.get_next_parameter()
+    logger.debug(tuner_params)
+    params = vars(merge_parameter(return_args, tuner_params))
+    print(params)
+
+    main(params)
